1c3342e50941999a6e0fdebc7f03b7ade864f03b
williamli250 <r36134191@gs.ncku.edu.tw>
2025-01-29 10:41:31 +0800

    刪除dash應用

--- code/app.py
+++
@@ -1,8 +0,0 @@
-from dash import Dash
-import dash_bootstrap_components as dbc
-
-app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
-server = app.server
-app.config.suppress_callback_exceptions = True
-
-
